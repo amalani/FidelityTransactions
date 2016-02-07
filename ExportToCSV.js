45445c09f02b11b7c8dbc885ac77efef24fbd0d2
Abhishek Malani <malani@gmail.com>
2016-02-06 20:52:35 -0800

    Base file to ExportToCSV

---
+++ ExportToCSV.js
@@ -0,0 +1,58 @@
+// WScript common code
+var console = {
+    log : function(s) { WScript.Echo(s); },
+    debug : function(s) { if (this.isDebugging) this.log(s); },
+    isDebugging : true
+};
+function alert(s) { console.log(s); }
+
+var context = {
+    scriptFileName : String(WScript.ScriptName).toLowerCase(),
+    scriptFolder : WScript.ScriptFullName.replace(WScript.ScriptName, ''), // has trailing slash
+    arguments : [],
+    loadArguments : function() {
+        for (var i = 0; count = WScript.Arguments.Count(), i < count; i++) {
+            context.arguments.push(WScript.Arguments.item(i))
+        }
+    }
+};
+context.loadArguments();
+
+// Common helpers
+// Finds an item in an array.
+if (!Array.prototype.indexOf) {
+   Array.prototype.indexOf = function(s) {
+      for (var i = 0; i < this.length; i++)
+      {
+         if (this[i].indexOf(s) > -1) return i;
+      }
+      return -1;
+   }
+}
+
+if (!String.prototype.trim) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+    String.prototype.trim = function () {
+        return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+    };
+}
+
+// Start script
+function CSVExporter() {
+    this.transactions = { 
+            range: '',          // transactions from date start to date end 
+            sources: [],        // types of sources
+            contributions : [], // [ {source, amount }], 
+            data : []           // [ { date, inv, type, amount, shares, tx : [source, amount, shares] } ] 
+    };
+}
+
+CSVExporter.prototype = {
+    constructor: CSVExporter,
+    run : function() {
+        console.log('dummy');
+    }
+}
+
+var exporter = new CSVExporter();
+exporter.run();
